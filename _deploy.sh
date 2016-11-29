7894f4dd014e8f5c45000958712d566783c1f425
karita <shigekikarita@gmail.com>
2016-11-30 00:04:19 +0900

    add deploy script

---
+++ _deploy.sh
@@ -0,0 +1,11 @@
+#!sh
+fname=`tempfile`_site
+jekyll build && \
+    cp -r _site $fname \
+    git checkout gh-pages \
+    ls | xargs rm -r \
+    cp -r $fname/* . \
+    git add . \
+    git commit -m "gen" \
+    git push origin gh-pages \
+    git checkout master
